e3bf665096fd3811524c33e095701a69fec7df3d
Nightre <nightscratchs@gmail.com>
2025-03-25 21:51:20 +0900

    Update remote-update.cmd

--- remote-update.cmd
+++ remote-update.cmd
@@ -4,7 +4,7 @@ set SERVER_USER=ubuntu
 set SCRIPT_PATH=/var/www/weird-factory/update.sh
 
 echo 连接到 %SERVER_USER%@%SERVER_IP%...
-ssh %SERVER_USER%@%SERVER_IP% "bash %SCRIPT_PATH%"
+ssh %SERVER_USER%@%SERVER_IP% "sudo bash %SCRIPT_PATH%"
 
 if %errorlevel% equ 0 (
   echo update.sh 更新成功
