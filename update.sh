f8e82bd386da8959702bc9ed32ec86d6747d069f
Nightre <nightscratchs@gmail.com>
2025-03-26 16:54:14 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd /var/www/weird-factory
+source ~/.nvm/nvm.sh  # 确保 NVM 可用
 nvm use 20
+export NODE_ENV=production
+echo "Current NODE_ENV: $NODE_ENV"
 git reset --hard HEAD
 git pull origin main
 cd server
 yarn
 cd ../client
 yarn
-pm2 restart w
+pm2 restart w --env production
 cd ..
 chmod +x /var/www/weird-factory/update.sh
-# pm2 start /var/www/weird-factory/server/bin/www --name wf
+pm2 start /var/www/weird-factory/server/bin/www --name wf --env production
